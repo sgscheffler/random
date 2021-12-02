c1781c254214a62c74bcd4bf863307a2866cbe46
Steven Scheffler <stevenscheffler@gmail.com>
2021-12-02 11:55:54 +0200

    Create verloop-chat.js

---
+++ verloop-chat.js
@@ -0,0 +1,19 @@
+// ==UserScript==
+// @name         Verloop Whatsapp Hacks
+// @namespace    https://travelstart.com/
+// @version      0.2
+// @description  try to take over the world!
+// @author       You
+// @match        https://travelstart.verloop.io/*
+// @icon         https://www.google.com/s2/favicons?domain=verloop.io
+// @grant       GM_addStyle
+// ==/UserScript==
+
+GM_addStyle ( `
+    #LivechatRoom_MessageBox_id {
+        resize: both;
+        height: 150px;
+        font-color: #000;
+        font-family:Arial;
+    }
+` );
